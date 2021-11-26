ede4d573525a713dae7cba24a74b5646dbde899f
Jordan Kimsey <jordankimsey@gmail.com>
2021-11-26 15:23:48 -0500

    removed if statement in Clouds.js

--- pages/components/Clouds.js
+++ pages/components/Clouds.js
@@ -1,21 +1,22 @@
 import React from 'react';
 
 const Clouds = (props) => {
-  if (props.cloud) {
-      const cloudArray = props.cloud;
+  // if (props.cloud) {
+    const cloudArray = props.cloud;
       
-        return (
-          <div>
-            <ul className='cloudArray'>
-              {cloudArray.map((cloud, index) => {
-                return <li key={index}>{cloud.repr}</li>;
-              })}
-            </ul>
-          </div>
-        );
-  } else {
-    let cloudArray = [];
-  }
+    return (
+      <div>
+        <ul className='cloudArray'>
+          {cloudArray.map((cloud, index) => {
+            return <li key={index}>{cloud.repr}</li>;
+          })}
+        </ul>
+      </div>
+    );
+  // }
+  // else {
+  //   let cloudArray = [];
+  // }
 
   // console.log(cloudArray[0].repr)
 
